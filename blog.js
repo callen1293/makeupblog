3a760e83ec801d1a15142b72c8ceed702cb699fa
Chris Allen <callen12193@gmail.com>
2018-09-05 21:10:51 -0700

    menu updated for mobile

--- blog.js
+++ blog.js
@@ -30,6 +30,12 @@ $(document).ready(function(){
       } 
   })
     
+    
+    $('.menu').click(function () {
+    $('html, body').animate({scrollTop:-1000}, 'fast');
+    return false;
+});
+
   
     
     
